3f0145e2f442c4b0a8ec258be2c1d3de8ffa516e
YouYoungSun <ffdr803@naver.com>
2025-07-02 21:54:43 +0900

    문구추가123

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("일단 시작해보자");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("후후");
         }
     }
 }
